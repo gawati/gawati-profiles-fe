76122c2d0cc17fe85edc9edc98e52f8318649795
Suraj Pratap Singh <spratapsingh11@gmail.com>
2018-05-28 23:30:09 +0000

    saved search edit and delete added

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -35,9 +35,15 @@ exports.searchSearch = async(req, res) => {
 exports.saveSearch = async(req, res) => {
   try{
     const data = req.body || {}
-    const search = new Search(data);
-    await search.save();
-    res.json({ "success": "true", "data": search});
+    const isSearch = await Search.findOne({ userName: req.body.userName, searchName: req.body.searchName});
+    if(isSearch){
+        winston.log("error", "saveSearch", {"message":"Search name is already present."});
+        res.json({"error": "saveSearch", "data": {"message":"Search name is already present."}});
+    }else{
+        const search = new Search(data);
+        await search.save();
+        res.json({ "success": "true", "data": search});
+    }
   }catch(err){
     winston.log("error", "saveSearch", err);
     res.json({"error": "saveSearch", "data": serializeError(err)});
@@ -56,4 +62,40 @@ exports.latestSearch = async(req, res) => {
         winston.log("error", "getSearch");
         res.json({"error": "getSearch", "data": serializeError(err)});
     }
+}
+
+exports.deleteSearch = async(req, res) => {
+    try{
+      const search = await Search.remove({_id: req.body._id});
+      if(search){
+        res.json({ "success": "true", "data": search});
+      }else{
+        res.json({ "success": "true", "data": {}});
+      }
+    }catch(err){
+        winston.log("error", "getSearch");
+        res.json({"error": "getSearch", "data": serializeError(err)});
+    }
+}
+
+exports.updateSearch = async(req, res) => {
+  try{
+    const data = req.body || {}
+    console.log(data);
+    console.log(data.searchName);
+    console.log(data.userName);
+    const isSearch = await Search.findOne({ userName: req.body.userName, searchName: req.body.searchName});
+    if(isSearch){
+        winston.log("error", "saveSearch", {"message":"Search name is already present."});
+        res.json({"error": "saveSearch", "data": {"message":"Search name is already present."}});
+    }else{
+        const search = await Search.findOne({ _id: req.body._id});
+        search.searchName = req.body.searchName;
+        await search.save();
+        res.json({ "success": "true", "data": search});
+    }
+  }catch (err) {
+    winston.log("error", "mongoError", err);
+    res.json({ "error": "mongoError", "data": serializeError(err)});
+  }
 }
